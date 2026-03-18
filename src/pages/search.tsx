816819961998509d95245d3b387f90040561a583
mevansweb <michelle.n.evans@gmail.com>
2026-03-17 20:29:05 -0400

    fix display of no result messages

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -14,35 +14,78 @@ const MIN_QUERY_LENGTH = 3
 
 const SEARCH_URL = 'http://localhost:8080/api/data/search/'
 
+const getJobsWithFutureEvents = (jobs: Job[]) => {
+  // Get today's date without time (midnight)
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  // Filter logic
+  const filteredData = jobs
+    .map(job => {
+      // Keep only events with date > today
+      const filteredDates = job.events.filter((event) => {
+        const date = new Date(event.date);
+        return !isNaN(date.getTime()) && date >= today;
+      });
+
+      // Return job only if it has future events
+      return filteredDates.length > 0
+        ? { ...job, events: filteredDates }
+        : null;
+    })
+    .filter((job): job is Job => job !== null); // Remove null entries
+  
+  return filteredData
+}
+
+const SearchResult = ({ job } : { job: Job }) => {
+  return (
+    <Card className="p-4 my-4">
+      <div key={job.id} className="mb-2">
+        <strong>{job.position}</strong> at {job.company} (Applied on: {new Date(job.applicationDate).toLocaleDateString()})
+        {job.linkToJobPosting ? <span> - <a href={job.linkToJobPosting} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">View Posting</a></span> : null}
+      </div>
+        <div className="text-sm light:text-gray-700 dark:text-gray-400">
+        {job.events && job.events.length > 0 ? (
+          <div>
+            <p className="text-sm font-light italic">Events:</p>
+            <ul className="list-disc list-inside text-sm light:text-gray-700 dark:text-gray-400">
+              {job.events.map((event, index) => {
+                if (event.status === 'waiting-for-response') {
+                  return (
+                    <li key={`${job.id}-event-${index}`}>{job.applicationDate ? `${new Date(job.applicationDate).toLocaleDateString()}: ` : ''}Application Sent.</li>
+                  )
+                } else {
+                  return (
+                    <li key={`${job.id}-event-${index}`}>{event.date ? `${new Date(event.date).toLocaleDateString()}: ` : ''}
+                      {capitalizeWords(event.status.replace(/-/g, ' '))} {event.note ? `- ${event.note}` : ''}
+                    </li>
+                  )
+                }
+              })}
+            </ul>
+          </div>
+        ) : (
+          null
+        )}
+      </div>
+    </Card>
+  )
+}
+
 const Search = () => {
   const { state } = useAuth()
   const { id, jobs } = state
   const [query, setQuery] = useState('')
   const timeoutRef = useRef<NodeJS.Timeout | null>(null)
-  const [searchResults, setSearchResults] = useState<Job[]>([])
+  const [searchResults, setSearchResults] = useState<Job[] | null>([])
+  const [upcoming, setUpcoming] = useState<Job[] | null>([])
 
   const getUpcomingEvents = useCallback(async () => {
-    // Get today's date without time (midnight)
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-
-    // Filter logic
-    const filteredData = jobs
-      .map(job => {
-        // Keep only events with date > today
-        const filteredDates = job.events.filter((event) => {
-          const date = new Date(event.date);
-          return !isNaN(date.getTime()) && date >= today;
-        });
-
-        // Return job only if it has future events
-        return filteredDates.length > 0
-          ? { ...job, events: filteredDates }
-          : null;
-      })
-      .filter((job): job is Job => job !== null); // Remove null entries
-
-    setSearchResults(filteredData)
+    const filteredData = getJobsWithFutureEvents(jobs)
+    setUpcoming(filteredData.length > 0 ? filteredData : null)
+    setSearchResults([])
+    setQuery('')
   }, [jobs])
 
   const performSearch = useCallback(async (searchQuery: string) => {
@@ -60,7 +103,8 @@ const Search = () => {
       }
 
       const result = await response.json()
-      setSearchResults(result)
+      setSearchResults(result.length > 0 ? result : null)
+      setUpcoming([])
     } catch (err) {
       const error = err as Error
       toast.error(`'Search error: ${error.message}`)
@@ -113,52 +157,29 @@ const Search = () => {
         >Get Upcoming Events</Button>
       </div>
 
-      {searchResults.length > 0 ? (
-        <div className="flex flex-col w-200 mx-auto">
-          {query ? (
-            <div className="text-sm text-gray-500 mt-8">Showing results for "{query}":</div>
-          ) : (
+      <div className="flex flex-col w-200 mx-auto">
+        {searchResults && searchResults.length > 0 || upcoming && upcoming.length > 0 ? (<h2 className="mt-4 text-lg font-semibold">Search Results:</h2>) : null}
+        {query ? (
+          <div className="text-sm text-gray-500 mt-8">Showing results for "{query}":</div>
+        ) : null}
+        <div className="mt-2">
+          {upcoming === null ? (
+            <p className="light:text-gray-700 dark:text-gray-400">No upcoming events found.</p>
+          ) : null}
+          {upcoming && upcoming.length > 0 ? (
             <div className="text-sm text-gray-500 mt-8">Showing upcoming events:</div>
-          )}
-          <h2 className="text-lg font-semibold">Search Results:</h2>
-          <div className="mt-2">
-            {searchResults.map((job) => (
-              <Card className="p-4 my-4" key={job.id}>
-              <div key={job.id} className="mb-2">
-                <strong>{job.position}</strong> at {job.company} (Applied on: {new Date(job.applicationDate).toLocaleDateString()})
-                {job.linkToJobPosting ? <span> - <a href={job.linkToJobPosting} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">View Posting</a></span> : null}
-              </div>
-               <div className="text-sm light:text-gray-700 dark:text-gray-400">
-                {job.events && job.events.length > 0 ? (
-                  <div>
-                    <p className="text-sm font-light italic">Events:</p>
-                    <ul className="list-disc list-inside text-sm light:text-gray-700 dark:text-gray-400">
-                      {job.events.map((event, index) => {
-                        if (event.status === 'waiting-for-response') {
-                          return (
-                            <li key={`${job.id}-event-${index}`}>{job.applicationDate ? `${new Date(job.applicationDate).toLocaleDateString()}: ` : ''}Application Sent.</li>
-                          )
-                        } else {
-                          return (
-                            <li key={`${job.id}-event-${index}`}>{event.date ? `${new Date(event.date).toLocaleDateString()}: ` : ''}
-                              {capitalizeWords(event.status.replace(/-/g, ' '))} {event.note ? `- ${event.note}` : ''}
-                            </li>
-                          )
-                        }
-                      })}
-                    </ul>
-                  </div>
-                ) : (
-                  null
-                )}
-              </div>
-              </Card>
-            ))}
-          </div>
+          ) : null}
+          {upcoming && upcoming.map((job) => (
+            <SearchResult key={`upcoming-events-job-${job.id}`} job={job} />
+          ))}
+          {searchResults && searchResults.map((job) => (
+            <SearchResult key={`search-results-job-${job.id}`} job={job} />
+          ))}
+          {searchResults === null ? (
+            query.trim().length >= MIN_QUERY_LENGTH && <p className="light:text-gray-700 dark:text-gray-400">No results found for "{query}".</p>
+          ) : null }
         </div>
-      ) : (
-        query.trim().length >= MIN_QUERY_LENGTH && <p className="light:text-gray-700 dark:text-gray-400">No results found for "{query}".</p>
-      )}
+      </div>
     </div>
   )
 }
