44ef08565a0128d7baafecb176b804e14807a80e
mevansweb <michelle.n.evans@gmail.com>
2026-04-17 20:54:37 -0400

    add sorting to subtasks

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 import { toast } from 'sonner'
 
@@ -6,6 +6,7 @@ import Header from '@/components/header'
 import { TasksModal } from '@/components/modal/tasks-modal'
 import { localStorageKey } from '@/components/providers/const'
 import { useAuth } from '@/components/providers/hooks'
+import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { Checkbox } from '@/components/ui/checkbox'
 import { spliceOrConcatArray } from '@/global/functions'
@@ -15,6 +16,10 @@ import type { Task, TaskEvent } from '@/global/types'
 const Tasks = () => {
   const { data, dispatch, error, existing, loading, postData, state } = useAuth()
   const allTasks = useMemo(() => state.tasks || [], [state.tasks])
+  const [sortSubtasksBy, setSortSubtasksBy] = useState<{
+    id: string
+    type: 'date' | 'alphabetical'
+  } | null>(null)
 
   const handleChange = useCallback(
     async ({ checked, task, subtask }: { checked: boolean; task: Task; subtask: TaskEvent }) => {
@@ -68,34 +73,76 @@ const Tasks = () => {
                   </div>
                   {task.events && task.events.length > 0 ? (
                     <div className="mt-2">
-                      <p className="text-sm font-light italic">Sub-Tasks:</p>
+                      <div className="mb-2 flex items-center justify-between gap-4 border-b-2 border-gray-200 py-2">
+                        <p className="text-sm font-light italic">
+                          Sub-Tasks ({task.events.length}):
+                        </p>
+                        <Button
+                          className="cursor-pointer px-2"
+                          variant="outline"
+                          onClick={() =>
+                            setSortSubtasksBy({
+                              id: task.id,
+                              type: 'date',
+                            })
+                          }
+                        >
+                          Sort by Date
+                        </Button>
+                        <Button
+                          className="cursor-pointer px-2"
+                          variant="outline"
+                          onClick={() =>
+                            setSortSubtasksBy({
+                              id: task.id,
+                              type: 'alphabetical',
+                            })
+                          }
+                        >
+                          Sort Alphabetically
+                        </Button>
+                      </div>
                       <ul className="light:text-gray-700 list-inside list-disc text-sm md:w-100 lg:w-150 dark:text-gray-400">
-                        {task.events.map((event, index) => (
-                          <li
-                            className="flex justify-between border-b border-dotted"
-                            key={`${task.id}-event-${index}`}
-                          >
-                            <div className="flex md:w-9/10 lg:w-9/10">
-                              <div className="md:w-1/5 lg:w-1/5">
-                                {event.dueDate
-                                  ? `${new Date(event.dueDate).toLocaleDateString()}: `
-                                  : ''}
-                              </div>
-                              <div className="flex md:w-4/5 lg:w-4/5">{event.note}</div>
-                            </div>
-                            <Checkbox
-                              checked={event.done}
-                              className="ml-4 border-2 border-blue-500 data-[state=checked]:bg-blue-500 data-[state=checked]:text-white dark:data-[state=checked]:bg-blue-900"
-                              onCheckedChange={(checked) =>
-                                handleChange({
-                                  checked: checked === true,
-                                  task,
-                                  subtask: event,
-                                })
+                        {task.events
+                          .sort((a, b) => {
+                            if (sortSubtasksBy?.id === task.id) {
+                              if (sortSubtasksBy.type === 'date') {
+                                const dateA = a.dueDate ? new Date(a.dueDate).getTime() : 0
+                                const dateB = b.dueDate ? new Date(b.dueDate).getTime() : 0
+                                return dateA - dateB
+                              } else {
+                                return a.note.localeCompare(b.note)
                               }
-                            />
-                          </li>
-                        ))}
+                            } else {
+                              return 0
+                            }
+                          })
+                          .map((event, index) => (
+                            <li
+                              className="flex justify-between border-b border-dotted"
+                              key={`${task.id}-event-${index}`}
+                            >
+                              <div className="flex md:w-9/10 lg:w-9/10">
+                                <div className="md:w-1/5 lg:w-1/5">
+                                  {event.dueDate
+                                    ? `${new Date(event.dueDate).toLocaleDateString()}: `
+                                    : ''}
+                                </div>
+                                <div className="flex md:w-4/5 lg:w-4/5">{event.note}</div>
+                              </div>
+                              <Checkbox
+                                checked={event.done}
+                                className="ml-4 border-2 border-blue-500 data-[state=checked]:bg-blue-500 data-[state=checked]:text-white dark:data-[state=checked]:bg-blue-900"
+                                onCheckedChange={(checked) =>
+                                  handleChange({
+                                    checked: checked === true,
+                                    task,
+                                    subtask: event,
+                                  })
+                                }
+                              />
+                            </li>
+                          ))}
                       </ul>
                     </div>
                   ) : null}
